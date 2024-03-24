40a82c7277f52a1851ca098e8c3aa06ff7b4a048
Sergio <sergiodinizdemiranda@gmail.com>
2024-03-24 11:12:08 -0300

    Atualização dos Repositorys

--- src/main/java/com/escola/api/escola/api/repositorys/AlunoRepository.java
+++ src/main/java/com/escola/api/escola/api/repositorys/AlunoRepository.java
@@ -1,6 +1,7 @@
 package com.escola.api.escola.api.repositorys;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +10,9 @@ import com.escola.api.escola.api.models.Aluno;
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
 
     List<Aluno> findAll();
+
+    Optional<Aluno> findByEmail(String email);
+
 
 
 }
